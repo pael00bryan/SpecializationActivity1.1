827ad131ee339150742ea47e554c8c221d6cfdd7
2ndyrGroupA <MY_NAME@example.com>
2021-09-02 09:07:28 +0800

    new file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> Create a simple HTML form and accept the user name and display the name through PHP echo statement.</h3>
+    <br>
+    <form action="" method="post">
+        <label for="username">Username: </label>
+        <input type="text" name="username">
+        <br>
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
